9e6b43f3dc96ff402f99c675ccb6df2ed35562a8
Filipe Patricio <facpatricio@gmail.com>
2024-09-10 16:29:31 +0100

    Add GithubService

--- Sources/MindMyLib/MindMyLib.swift
+++ Sources/MindMyLib/MindMyLib.swift
@@ -1,2 +1,29 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Combine
+import Foundation
+
+@available(iOS 13.0, *)
+public class GithubService {
+    public init() {}
+    public func getOrganizations() throws -> AnyPublisher<Data, Error> {
+        let urlString = "https://api.github.com/organizations"
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { output -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300
+                else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
